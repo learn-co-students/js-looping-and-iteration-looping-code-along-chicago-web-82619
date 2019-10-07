9be8e3bad4cae814246293b26053303f3a5ffca0
zSteinO34 <zstein1234@gmail.com>
2019-10-07 10:37:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(arr, string) {
+    let array = []
+    for(let i = 0; i < arr.length; i++) {
+        array.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    }
+    return array;
+}
+
+function countdown(num) {
+    let count = num;
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    }
+}
