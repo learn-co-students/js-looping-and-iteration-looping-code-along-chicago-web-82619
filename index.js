ff80a778e3cb08ef1684f105b5bad536b6bcafc0
justrmarks <blackbirdwanderer@gmail.com>
2019-10-07 10:38:19 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+  let result = [];
+  for (let i=0; i< names.length; i++) {
+    result.push("Thank you, " + names[i] + ", for the wonderful surprise gift!")
+    console.log(result[i])
+  }
+  return result;
+}
+
+function countdown(x) {
+  let i = x;
+  while(i >= 0) {
+    console.log(i)
+    i--;
+  }
+}
